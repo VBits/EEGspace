98b9fa99d9985c333819dee5f55de5f3cc20239b
VBits <v.bitsikas@gmail.com>
2021-07-11 18:48:12 +0200

    MG: tried a few smoothing algortihms.

--- VaeSmoothing.py
+++ VaeSmoothing.py
@@ -275,6 +275,118 @@ def try_predict_class(multitaper, states, load_weights=False):
 
     print("done")
 
+from statsmodels.tsa.api import ExponentialSmoothing, SimpleExpSmoothing, Holt
+from scipy.signal import savgol_filter
+
+
+def my_filter(x):
+    return savgol_filter(x, 13, 1)
+
+def try_smoothing_function(multitaper, smoothed, states, smoothing_function):
+
+    # fit = SimpleExpSmoothing(multitaper, initialization_method="heuristic").fit(smoothing_level=0.2, optimized=False)
+    multitaper = np.array(multitaper)
+    smoothed = np.array(smoothed)
+    training_data_states = np.array([s[0] for s in np.array(states)])
+    smoothed_by_function = []
+
+    for i in range(0, len(multitaper)):
+        epoch = smoothing_function(multitaper[i]) #SimpleExpSmoothing(multitaper[i], initialization_method="heuristic").fit(smoothing_level=0.05, optimized=False).fittedvalues
+        smoothed_by_function.append(epoch)
+        #fit = SimpleExpSmoothing(multitaper[i], initialization_method="heuristic").fit(smoothing_level=0.05, optimized=False)
+
+        #fit = SimpleExpSmoothing(multitaper[i], initialization_method="estimated").fit()
+
+
+    n = 10  # How many digits we will display
+    plt.figure(figsize=(30, 6))
+    for i in range(n):
+        idx = i + 20000
+
+        # Display original
+        ax = plt.subplot(3, n, i + 1)
+        plt.plot(multitaper[i])
+        plt.gray()
+
+        # Display original
+        ax = plt.subplot(3, n, i + 1 + n)
+        plt.plot(np.array(smoothed_by_function[i]))
+        plt.gray()
+
+        # Display reconstruction
+        ax = plt.subplot(3, n, i + 1 + (2 * n))
+        plt.plot(smoothed[i])
+        plt.gray()
+
+    plt.show()
+
+    lda = LDA(n_components=3)
+    lda_encoded_data = lda.fit_transform(smoothed, training_data_states)
+    plot_transformation = True
+    if plot_transformation:
+        fig = plt.figure()
+        ax = Axes3D(fig)
+        d = {0: 'blue',  # lm
+             1: 'yellow',  # sws
+             2: 'green',  # hmw
+             3: 'red',  # rem
+             }
+        colors = [d[c] for c in np.array(training_data_states)]
+        ax.scatter(lda_encoded_data[:, 0], lda_encoded_data[:, 1], lda_encoded_data[:, 2], c=colors, alpha=0.1, s=8)
+        ax.set_xlabel('component 1')
+        ax.set_ylabel('component 2')
+        ax.set_zlabel('component 3')
+        plt.show()
+
+    print("done")
+    return smoothed_by_function
+
+def get_average_of_classes(multitaper, states):
+    states = np.array(states)
+    cluster_indexes = [np.where(states[:, 1] == "LMwake"), np.where(states[:, 1] == "SWS"),
+                       np.where(states[:, 1] == "HMwake"), np.where(states[:, 1] == "REM")]
+    averages = []
+    for i in range(len(cluster_indexes)):
+        class_examples = np.array(multitaper)[cluster_indexes[i]]
+        averages.append(np.mean(class_examples, axis=0))
+
+    plt.figure(figsize=(10, 6))
+    n = len(averages)
+    for i in range(n):
+        idx = i + 20000
+
+        # Display original
+        ax = plt.subplot(1, n, i + 1)
+        plt.plot(averages[i])
+        plt.gray()
+
+    plt.show()
+
+    return averages
+
+from statsmodels.tsa.arima.model import ARIMA
+
+def do_nothing(x):
+    return x
+
+def arima(x):
+    model = ARIMA(x, order=(5, 1, 0))
+    model_fit = model.fit()
+    return model_fit.fittedvalues
+
+def moving_average(x):
+    return np.convolve(x, np.ones(15)/15, mode='valid')
+
+def ses_smoothing(x):
+    #return SimpleExpSmoothing(x, initialization_method="estimated").fit().fittedvalues
+    return SimpleExpSmoothing(x, initialization_method="heuristic").fit(smoothing_level=0.05, optimized=False).fittedvalues
+
+def savgol_smoothing(x):
+    savgol_smoothed_segment = x
+    for iteration in range(4):
+        savgol_smoothed_segment = my_filter(savgol_smoothed_segment)
+    return savgol_smoothed_segment
+
 def get_multitaper_data():
     mh = Offline_analysis.run_offline_analysis()
     return {
@@ -283,14 +395,42 @@ def get_multitaper_data():
                  "smoothed": mh.Sxx_norm
             }
 
+def smooth_data(x, smoothing_function):
+    x = np.array(x)
+    smoothed_by_function = []
+    for i in range(0, len(x)):
+        epoch = smoothing_function(x[i])  # SimpleExpSmoothing(multitaper[i], initialization_method="heuristic").fit(smoothing_level=0.05, optimized=False).fittedvalues
+        smoothed_by_function.append(epoch)
+    return smoothed_by_function
+
+def compare_against_averages(x, states, averages):
+    x = np.array(x)
+    states = np.array([s[0] for s in np.array(states)])
+    new_states = []
+    for i in range(0, len(x)):
+        fit_numbers = []
+        for j in range(0, len(averages)):
+            fit_numbers.append([np.linalg.norm(x[i] - averages[j])])
+        min_item = np.min(fit_numbers)
+        min_index = fit_numbers.index(min_item)
+        new_states.append(min_index)
+    return (len([i for i in range(len(states)) if states[i] == new_states[i]])/len(states)) * 100
+
 multitaper_data_path = Config.base_path + "/vae_smoothing_trail_mouse_20210612.pkl"
 data = Storage.load_or_recreate_file(multitaper_data_path, get_multitaper_data, False)
 multitaper = data["multitaper"]
 unsmoothed = data["unsmoothed"]
 smoothed = data["smoothed"]
-run_keras_var_autoencoder(load_weights=False, mouse_data=multitaper)
+#run_keras_var_autoencoder(load_weights=False, mouse_data=multitaper)
 states = Storage.load_from_file("C:/Users/bitsik0000/SleepData/210409/Ephys/states_210409_210409_B6J_m1.pkl")
-run_keras_basic_autencoder(smoothed, states, False)
+averages = get_average_of_classes(smoothed, states)
+difference = compare_against_averages(smoothed, states, averages)
+print(difference)
+smoothed_by_function = smooth_data(smoothed, ses_smoothing)
+transformed = try_smoothing_function(unsmoothed, smoothed_by_function, states, do_nothing)
+_ = get_average_of_classes(transformed, states)
+#run_keras_basic_autencoder(smoothed, states, False)
+
 #try_output_smoothed(unsmoothed, smoothed, states)
 #try_predict_class(unsmoothed, states)
 # check_labelled_data_on_autoencoder(multitaper_df, states)
