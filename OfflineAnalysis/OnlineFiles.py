d8f67f23fdfc175497e752f97711f25fca469d44
Matthew.Grant <Matthew.Grant@ovivowater.com>
2022-04-17 14:58:24 +0200

    Bad first cut of the state mappings, next will be to predict the class from the knn.

--- OfflineAnalysis/OnlineFiles.py
+++ OfflineAnalysis/OnlineFiles.py
@@ -2,6 +2,7 @@ import Common
 import Storage
 import Config
 from OnlineAnalysis import Config as OnlineConfig
+from OfflineAnalysis import Config as OfflineConfig
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 import numpy as np
@@ -26,6 +27,29 @@ def create_required_files_for_online_mode(mouse_num):
     series = combined_data[:set_size]
     states_numeric = states_numeric[mid:set_size + mid]
     Storage.dump_to_file(OnlineConfig.combined_data_file_path.format(mouse_num=mouse_num), combined_data)
+
+    saved_state_averages = Storage.load_from_file(OfflineConfig.state_averages_path)
+    data_state_averages = pd.DataFrame()
+    for label in np.unique(states['clusters_knn']):
+        data_state_averages[label] = multitaper_df.loc[states[states['clusters_knn'] == label].index].mean(axis=0)
+
+    state_mappings = {}
+    for data_state_average in data_state_averages:
+        lowest_error = None
+        state_mappings[data_state_average] = None
+        for saved_state_average in saved_state_averages:
+            error = sklearn.metrics.mean_squared_error(data_state_average, saved_state_average)
+            if lowest_error is None or error < lowest_error:
+                lowest_error = error
+                state_mappings[data_state_average] = saved_state_average
+
+
+    #load the state averages for the data that was used with the knn
+    #get the averages of the states for the data that is loaded up
+    #compare the two using the least error between the two?
+    #find the label for the data average add it as the key in a dict
+    #find the label of the matching knn data average add this as the value
+    #when all is done add this into the state mapping file
 
     lda = Storage.load_from_file(OnlineConfig.lda_file_path.format(mouse_num=mouse_num))
     lda_transformed = lda.transform(series)
